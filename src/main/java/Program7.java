743a51df422a13712658f472f135abbec99cdf7a
Abeysinghe_A <151038579+avinAbeysinghe@users.noreply.github.com>
2024-08-26 20:45:57 -0700

    Update Program7.java
    Completed program 7

--- src/main/java/Program7.java
+++ src/main/java/Program7.java
@@ -1,30 +1,47 @@
 package src.main.java;
-//import stuff here
-
-//Your code here
-
-public class Program7{
-	//Define your final variables here
-	// **HINT: Maybe the conversion rates?????**
+import java.util.Scanner; 
 
 
-	public static void main(String[] args){
-		//Make you own test code here
-		Program7 tester = new Program7();
-		
-		double myTestVal = 0.0;
-		
-		myTestVal = tester.convertMoney(0,0,0);
-		
-		System.out.println(myTestVal);
-	}
-	
-	
-	
-	public double convertMoney(int schruteBucks, int stanleyNickels, int klevins){
-		//implement a function that converts from the old format to the new format
-		
-	}
-	
-	
+
+public class Program7
+{
+    //klevin -> schrute buck
+    final double klevinConvert(int k){
+        double decimal = (double)k / 20;
+        return decimal;
+    }
+    //stanley -> shrute buck
+    final double snConvert(int sn){
+        double decimal = (double)sn / 12 / 20; 
+        return decimal;
+    }
+    
+
+    public static void main(String[] args){
+        int schruteBuck, klevin, stanleyNickel;
+        double myTestVal = 0.0;
+        Program7 tester = new Program7();
+        Scanner reader = new Scanner(System.in);
+        
+        
+        
+        System.out.println("Enter schrute-bucks: ");
+        schruteBuck = reader.nextInt();
+        System.out.println("Enter klevins: ");
+        klevin = reader.nextInt();
+        System.out.println("Enter stanley-nickels: ");
+        stanleyNickel = reader.nextInt();
+        
+        myTestVal = tester.convertMoney(schruteBuck,stanleyNickel,klevin);
+        
+        System.out.println("Decimal schrute-bucks: $"+ myTestVal);
+    }
+    
+    
+    public double convertMoney(int schruteBucks, int stanleyNickels, int klevins){
+        double conversion = (double)schruteBucks + klevinConvert(klevins) + snConvert(stanleyNickels);
+        return conversion;
+    }
+    
+    
 }
